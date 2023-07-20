062ab642029e7e2ce43e9d64d087cf1dddd6c5b8
baranisdharan <110192626+baranisdharan@users.noreply.github.com>
2023-07-20 20:18:01 +0530

    Expense full stack

---
+++ routes/expense.js
@@ -0,0 +1,13 @@
+const express=require('express')
+const router=express.Router()
+const expenseController=require('../controllers/expense')
+const Expense=require('../models/Expense')
+
+
+router.post('/expense/add-expense',expenseController.addExpense)
+router.get('/expense/get-expense',expenseController.getExpense)
+router.delete('/expense/delete-expense/:id',expenseController.deleteExpense)
+
+
+module.exports=router
+  
